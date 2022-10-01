d298ea4c26fafb8ce7f12eb2db11e1bd89046044
DivideCo <micahvdm@gmail.com>
2022-10-01 21:55:02 +0200

    fix with correct ip lookup

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-ipaddr = ifconfig wlan0 | awk '/inet/{print substr($2,5)}'
+ipaddr=`hostname -I`
 echo $ipaddr
 
 sudo apt install -y liblilv-dev libboost-dev jackd2 libjack-jackd2-dev libnl-3-dev libnl-genl-3-dev libsystemd-dev catch libasound2-dev uuid-dev libwebsocketpp-dev authbind libavahi-client-dev
