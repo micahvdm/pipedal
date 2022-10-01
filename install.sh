212c41c42b2e6087387f51e39f55d674b24284f2
DivideCo <micahvdm@gmail.com>
2022-10-01 21:50:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 set -x
 
 ipaddr = ifconfig wlan0 | awk '/inet/{print substr($2,5)}'
+echo $ipaddr
 
 sudo apt install -y liblilv-dev libboost-dev jackd2 libjack-jackd2-dev libnl-3-dev libnl-genl-3-dev libsystemd-dev catch libasound2-dev uuid-dev libwebsocketpp-dev authbind libavahi-client-dev
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
